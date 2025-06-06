b2d6359797982a2c54b2a3f0d914d2ed18c8e9e1
Riley <koppril7@gmail.com>
2025-06-06 16:56:23 -0400

    Kill player and reset game on hit

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,9 +2,12 @@ extends Node
 
 @export var obstacle_scene: PackedScene
 
+var player_start_pos = Vector2.ZERO
+var obstacles = []
+
 func _ready():
 	$player.start_game.connect(on_start_game)
-	var player_start_pos = Vector2.ZERO
+	
 	player_start_pos.y = get_viewport().get_visible_rect().size.y/2
 	player_start_pos.x = 50
 	
@@ -18,11 +21,10 @@ func on_spawn_tick_expired():
 	mob.position.y = get_viewport().get_visible_rect().size.y
 	mob.position.x = get_viewport().get_visible_rect().size.x + 32
 	
-	
+	obstacles.append(mob)
 	add_child(mob)
-	
 
-
-		
-
-	
+func _on_player_hit() -> void:
+	$player.position = player_start_pos
+	for mob in obstacles:
+		mob.queue_free()
