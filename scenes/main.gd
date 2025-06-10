f28009b20aedbdff8b51b0ff25d3d933d0e940dc
Riley <koppril7@gmail.com>
2025-06-10 07:05:04 -0400

    Add upper pipe

--- scenes/main.gd
+++ scenes/main.gd
@@ -17,9 +17,19 @@ func on_start_game():
 	$ObstacleSpawnTick.start()
 	
 func on_spawn_tick_expired():
+	var offset = randf_range(0,25)
+	var spacing_adjustment = randi_range(0, 15)
 	var mob = obstacle_scene.instantiate()
-	mob.position.y = get_viewport().get_visible_rect().size.y
+	mob.position.y = get_viewport().get_visible_rect().size.y + offset
 	mob.position.x = get_viewport().get_visible_rect().size.x + 32
+	
+	obstacles.append(mob)
+	add_child(mob)
+	
+	mob = obstacle_scene.instantiate()
+	mob.position.y = 0 + offset - spacing_adjustment
+	mob.position.x = get_viewport().get_visible_rect().size.x + 32
+	mob.rotation = PI
 	
 	obstacles.append(mob)
 	add_child(mob)
@@ -28,6 +38,7 @@ func _on_player_hit() -> void:
 	$ObstacleSpawnTick.stop()
 	$player.position = player_start_pos
 	for mob in obstacles:
-		mob.queue_free()
+		if mob != null:
+			mob.queue_free()
 	
 	obstacles.clear()
